d91ab5ceff5f95643f9fbeadc8408f7cfc198032
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-01 23:57:12 -0300

    Was added endpoint to create passengers

--- src/passenger/passenger.service.ts
+++ src/passenger/passenger.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IPassenger } from 'src/common/interfaces/passenger.interface';
+import { PASSENGER } from 'src/common/models/models';
+import { PassengerDTO } from './dto/passenger.dto';
 
 @Injectable()
-export class PassengerService {}
+export class PassengerService {
+    constructor(@InjectModel(PASSENGER.name) private readonly model: Model<IPassenger>) { }
+
+    async create(passengerDTO: PassengerDTO): Promise<IPassenger> {
+        const newPassenger = new this.model(passengerDTO);
+
+        return await newPassenger.save();
+    }
+}
