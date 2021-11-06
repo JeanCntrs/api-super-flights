9deb1c37f6068d8036995eb3f41b8fc4f54eeee3
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-06 12:22:56 -0300

    Was added endpoint to add passenger to flight

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,15 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { PassengerService } from 'src/passenger/passenger.service';
 import { FlightDTO } from './dto/flight.dto';
 import { FlightService } from './flight.service';
 
 @Controller('api/v1/flights')
 export class FlightController {
-    constructor(private readonly flightService: FlightService) { }
-
-    @Post()
-    create(@Body() flightDTO: FlightDTO) {
-        return this.flightService.create(flightDTO);
-    }
+    constructor(
+        private readonly flightService: FlightService,
+        private readonly passengerService: PassengerService
+    ) { }
 
     @Get()
     findAll() {
@@ -19,6 +18,25 @@ export class FlightController {
     @Get(':id')
     findOne(@Param('id') id: string) {
         return this.flightService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() flightDTO: FlightDTO) {
+        return this.flightService.create(flightDTO);
+    }
+
+    @Post(':flightId/passenger/:passengerId')
+    async addPassenger(
+        @Param('flightId') flightId: string,
+        @Param('passengerId') passengerId: string
+    ) {
+        const passenger = await this.passengerService.findOne(passengerId);
+
+        if (!passenger) {
+            throw new HttpException('Passenger not found', HttpStatus.NOT_FOUND);
+        }
+
+        return this.flightService.addPassenger(flightId, passengerId);
     }
 
     @Put(':id')
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -9,18 +9,26 @@ import { FlightDTO } from './dto/flight.dto';
 export class FlightService {
     constructor(@InjectModel(FLIGHT.name) private readonly model: Model<IFlight>) { }
 
-    async create(flightDTO: FlightDTO): Promise<IFlight> {
-        const newFlight = new this.model(flightDTO);
-
-        return await newFlight.save();
-    }
-
     async findAll(): Promise<IFlight[]> {
         return await this.model.find();
     }
 
     async findOne(id: string): Promise<IFlight> {
         return await this.model.findById(id);
+    }
+
+    async create(flightDTO: FlightDTO): Promise<IFlight> {
+        const newFlight = new this.model(flightDTO);
+
+        return await newFlight.save();
+    }
+
+    async addPassenger(flightId: string, passengerId: string): Promise<IFlight> {
+        return await this.model.findByIdAndUpdate(flightId, {
+            $addToSet: { passengers: passengerId }
+        },
+            { new: true }
+        ).populate('passengers');
     }
 
     async update(id: string, flightDTO: FlightDTO): Promise<IFlight> {
