44566182a767ab94d1b9dbf7be4c0e980eb44c8f
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-07 11:17:45 -0300

    Was added the metaweather api to get and return the weather

--- src/common/interfaces/flight.interface.ts
+++ src/common/interfaces/flight.interface.ts
@@ -2,6 +2,7 @@ import { IPassenger } from "./passenger.interface";
 import { IWeather } from "./weather.interface";
 
 export interface IFlight extends Document {
+    _id?: string;
     pilot: string;
     airplane: string;
     destinationCity: string;
--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -7,6 +7,7 @@ import { FlightDTO } from './dto/flight.dto';
 import * as moment from 'moment'
 import axios from 'axios';
 import { ILocation } from 'src/common/interfaces/location.interface';
+import { IWeather } from 'src/common/interfaces/weather.interface';
 @Injectable()
 export class FlightService {
     constructor(@InjectModel(FLIGHT.name) private readonly model: Model<IFlight>) { }
@@ -18,8 +19,9 @@ export class FlightService {
     async findOne(id: string): Promise<IFlight> {
         const flight = await this.model.findById(id).populate('passengers');
         const location: ILocation = await this.getLocation(flight.destinationCity);
+        const weather: IWeather[] = await this.getWeather(location.woeid, flight.flightDate);
 
-        return flight;
+        return this.assign(flight, weather);
     }
 
     async create(flightDTO: FlightDTO): Promise<IFlight> {
@@ -50,5 +52,28 @@ export class FlightService {
         const { data } = await axios.get(`https://www.metaweather.com/api/location/search/?query=${destinationCity}`);
 
         return data[0];
+    }
+
+    async getWeather(woeid: number, flightDate: Date): Promise<IWeather[]> {
+        const dateFormat = moment.utc(flightDate).format();
+        const year = dateFormat.substring(0, 4);
+        const month = dateFormat.substring(5, 7);
+        const day = dateFormat.substring(8, 10);
+
+        const { data } = await axios.get(`https://www.metaweather.com//api/location/${woeid}/${year}/${month}/${day}`);
+
+        return data;
+    }
+
+    assign(flight: IFlight, weather: IWeather[]): IFlight {
+        return Object.assign({
+            _id: flight._id,
+            pilot: flight.pilot,
+            airplane: flight.airplane,
+            destinationCity: flight.destinationCity,
+            flightDate: flight.flightDate,
+            passengers: flight.passengers,
+            weather
+        });
     }
 }
